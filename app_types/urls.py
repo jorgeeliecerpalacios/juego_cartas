cc946946f63e3af5b8d8b0c96c40b2951fb0ccef
jorgeeliecerpalacios <jorge_palacios_1987@hotmail.com>
2024-06-16 16:40:50 -0500

    create route and add bootstrapp

---
+++ app_types/urls.py
@@ -0,0 +1,15 @@
+# urls app_types
+
+from django.urls import path
+
+# Views
+
+from app_types import views
+
+urlpatterns = [
+    path(
+        # route='', 
+        # view=views.list_posts, 
+        # name='feed'
+        ),
+]
